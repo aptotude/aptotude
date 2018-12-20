9dc45e0a5978054adc2c9b000a2ada96849f469d
James Olson <jolson@apto.com>
2018-12-19 19:44:05 -0700

    fix package

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,7 +12,7 @@ git config user.email "$GIT_EMAIL"
 git config user.name "$GIT_USER"
 
 # Get the latest version from NPM
-LATEST_RELEASE_VERSION=$(npm view aptotude version)
+LATEST_RELEASE_VERSION=$(npm view @apto/aptotude version)
 
 echo "Latest npm version $LATEST_RELEASE_VERSION"
 
