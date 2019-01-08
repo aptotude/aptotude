33573c3c074c8322e082497acf87d5af5daa01bf
Adam McCune <adam@redpointcrm.com>
2019-01-08 15:14:53 -0700

    MICRO-19 Added navbar component

---
+++ src/components/Navbar/AptoNavbar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import classNames from 'classnames';
+import './aptoNavbar.scss';
+import { StandardTypes } from '../../utils/standardTypes';
+
+export interface AptoNavbarDisplayProps extends StandardTypes {
+    fixed?: boolean | undefined;
+}
+
+const COMPONENT_PREFIX = 'AptoNavbar';
+
+export class AptoNavbar extends React.PureComponent<AptoNavbarDisplayProps> {
+    public render() {
+        const {
+            fixed,
+            className,
+            forwardRef,
+            children,
+            ...rest
+        } = this.props;
+
+        const classes = classNames(
+            COMPONENT_PREFIX,
+            fixed && `${COMPONENT_PREFIX}--fixed`,
+            className
+        );
+
+        return (
+            <div
+                {...rest}
+                ref={forwardRef}
+                className={classes}>
+                <div className="AptoNavbar-content">
+                    {children}
+                </div>
+            </div>
+        );
+    }
+}
