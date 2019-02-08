a70dafc1ae3c64376aff2a4e209f9320cb11a45f
James Olson <jolson@apto.com>
2019-02-07 17:53:55 -0700

    update search component exports

--- src/components/Search/AptoSearch.tsx
+++ src/components/Search/AptoSearch.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './aptoSearch.scss';
-import { Search, SearchProps } from 'semantic-ui-react';
+import { Search, SearchProps as AptoSearchProps } from 'semantic-ui-react';
 import { AptoIcon } from '../Icon';
 import { searchIcon } from './SearchIcon';
 import { AptoSpinner } from '../Spinner';
 
-export class AptoSearch extends React.Component<SearchProps> {
+export { SearchProps as AptoSearchProps } from 'semantic-ui-react';
+
+export class AptoSearch extends React.Component<AptoSearchProps> {
   public render() {
     const { loading, ...rest } = this.props;
     return (
