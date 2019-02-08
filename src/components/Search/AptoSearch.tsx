c9625a93492aeaefd4ca696909fd9cbe920b07ed
James Olson <jolson@apto.com>
2019-02-07 17:36:58 -0700

    add correct props

--- src/components/Search/AptoSearch.tsx
+++ src/components/Search/AptoSearch.tsx
@@ -5,36 +5,12 @@ import { AptoIcon } from '../Icon';
 import { searchIcon } from './SearchIcon';
 import { AptoSpinner } from '../Spinner';
 
-interface AptoSearchProps {
-  loading: boolean;
-  results: any;
-  value?: string;
-  onResultSelect: (
-    e: React.MouseEvent<HTMLDivElement>,
-    data: SearchProps
-  ) => void;
-  onSearchChange: (e: React.MouseEvent<HTMLElement>, data: SearchProps) => void;
-}
-
-export class AptoSearch extends React.Component<AptoSearchProps> {
+export class AptoSearch extends React.Component<SearchProps> {
   public render() {
-    const {
-      loading,
-      results,
-      value,
-      onResultSelect,
-      onSearchChange
-    } = this.props;
+    const { loading, ...rest } = this.props;
     return (
       <div className="AptoSearch">
-        <Search
-          loading={loading}
-          results={results}
-          value={value}
-          onResultSelect={onResultSelect}
-          onSearchChange={onSearchChange}
-          input={{ icon: null }}
-        />
+        <Search input={{ icon: null }} {...rest} />
         {!loading && <AptoIcon icon={searchIcon} />}
         {loading && <AptoSpinner noOverlay={true} />}
       </div>
